b0155d209d168e1979aabf557cd695d4fd229eeb
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2023-03-26 14:41:47 +0500

    commit after Q5

---
+++ Q3.ts
@@ -0,0 +1,19 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let PersonName1 :string ="muazam mughal";
+// in the lower case
+let upper :string = PersonName1.toUpperCase();
+console.log(upper);
+
+// to lower case
+
+let lower = PersonName1.toLowerCase();
+console.log(lower)
+
+//to title case
+
+console.log(PersonName1.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' '));
+
+//in this title case we split the array by space then using map funtion, itreate bothwords by our conditions and using the join function 
+//combine the both split words in one
+ 
---
+++ Q5.ts
@@ -0,0 +1,7 @@
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+ Then compose your message and store it in a new variable called message. Print your message.*/
+
+ let famousPerson : string = "Nelson Mandela"
+ let theQoute :string ='"Defination of stupid : Knowing the Truth, Seeing the Truth, still Believing in Lies.. "';
+
+ console.log(famousPerson +" " + "once said," +" " + theQoute);
