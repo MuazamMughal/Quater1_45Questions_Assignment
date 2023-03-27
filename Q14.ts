b2b14197acc82f76cdc2e390957c705524e90858
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2023-03-27 17:53:23 +0500

    Update Q14.ts

--- Q14.ts
+++ Q14.ts
@@ -3,3 +3,16 @@ Guest List: If you could invite anyone, living or deceased, to dinner, who would
  least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 */
 
+// Create an array of guests
+const guestList: string[] = ["Albert Einstein", "Oprah Winfrey", "Leonardo da Vinci","Imran Khan"];
+
+// Loop through the array and print an invitation message to each guest using the for each function
+
+guestList.forEach(guest => {
+
+// guest in this represents the each particepents of list
+ 
+  console.log(`Dear ${guest},\n You are invited to dinner at my place. Please join me for an evening of good food and great conversation.
+  \nSincerely,\nMuazm Mughal\n`);
+
+});
